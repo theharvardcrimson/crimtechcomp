56d22dc9871977801e6279e9020bef02a1de9e18
Sara Kvaska <sarakvaska@Saras-MacBook-Pro-3.local>
2018-02-09 15:19:55 -0500

    CrimTechComp 000

--- assignments/000-codesarakvaska/assignment.py
+++ assignments/000-codesarakvaska/assignment.py
@@ -1,45 +1,48 @@
-print("Hello, world!")
+def say_hello():
+    print("Hello, world!")
 
-"""
-
-# this is a singleline comment, PLEASE EDIT ME
-print("I am not executed when in the triple quotes, but once you remove them...PLS REMOVE ME K THX BAI")
+# Color: Blue
 
 # TODO: implement
 def echo_me(msg):
+    raw_input = msg
+    print(msg)
 
-# TODO: understand and remove
+# TODO: understand and remove ???????
 def string_or_not(d):
     exec(d)
 
 # TODO: understand formatting - can you eliminate the redundancy here?
 def append_msg(msg):
-    print("Your message was: {}.".format(msg))
+    print("Your message should have been: {}!".format(msg))
 
 # TODO: understanding classes (an introduction)
 class QuickMaths():
     def add(self, x, y):
-        return None
+        return x + y
 
     def subtract(self, x, y):
-        return None
+        return x - y
 
     def multiply(self, x, y):
-        return None
+        return x * y
 
     def divide(self, x, y):
-        return None
+        return x / y
 
 # TODO: implement - can you do this more efficiently?
 def increment_by_one(lst):
     new_lst = list()
 
     for x in lst:
-        new_lst.append(x - 1)
+        new_lst.append(x + 1)
 
     return new_lst
 
 # TODO: understand - do we need a return statement here? why?
+# We do need a return statement if we want the program to return/show the person's name after
+# someone chooses to update the name in the dictioary. Otherwise, we don't need a
+# return statement
 def update_name(person, new_name):
     person["name"] = new_name
 
@@ -47,9 +50,13 @@ def update_name(person, new_name):
 
 # TODO: implement - these are still required, but are combinations of learned skills + some
 def challenge1(lst):
-    return None
+    new_list = lst.reverse()
+    return new_list
 
-# TODO: implement
+# TODO: implement (return divisors in a list)
 def challenge2(n):
-    return None
-"""
+    factors = list()
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            factors.append((i, n / i))
+    return factors
