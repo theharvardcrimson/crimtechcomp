dbd827f79c59ad0bac3116e0e12fb5d509d022ce
sepulchre01 <36139072+sepulchre01@users.noreply.github.com>
2018-02-11 22:53:28 -0500

    Update assignment.py

--- assignments/000/000-code/assignment.py
+++ assignments/000/000-code/assignment.py
@@ -1,79 +1,40 @@
-def say_hello():
-    print("Hello, world!")
+print("Hello, world!")
 
-
-# Color: Blue
-
+"""
+# this is a singleline comment, PLEASE EDIT ME
+print("I am not executed when in the triple quotes, but once you remove them...PLS REMOVE ME K THX BAI")
 # TODO: implement
 def echo_me(msg):
-	print(msg)
-
-
-
+# TODO: understand and remove
+def string_or_not(d):
+    exec(d)
 # TODO: understand formatting - can you eliminate the redundancy here?
 def append_msg(msg):
-    print("Your message should have been: " + msg + "!")
-
+    print("Your message was: {}.".format(msg))
 # TODO: understanding classes (an introduction)
 class QuickMaths():
     def add(self, x, y):
         return None
-
     def subtract(self, x, y):
         return None
-
     def multiply(self, x, y):
         return None
-
     def divide(self, x, y):
         return None
-
 # TODO: implement - can you do this more efficiently?
 def increment_by_one(lst):
     new_lst = list()
-
     for x in lst:
-        new_lst.append(x + 1)
-
+        new_lst.append(x - 1)
     return new_lst
-
 # TODO: understand - do we need a return statement here? why?
 def update_name(person, new_name):
     person["name"] = new_name
-
     return person
-
 # TODO: implement - these are still required, but are combinations of learned skills + some
 def challenge1(lst):
-    new_list = list()
-    m = range(len(lst))
-    l = len(lst) - 1
-    for num in m:         #iterates thru each word in lst, starting with the last one
-        word = lst[l]
-        new_word = ""
-        i = len(word) - 1
-        for n in range(len(word)): #iterates thru each letter in word, starting with the last one
-            new_word += word[i]
-            i-= 1
-        new_list.append(new_word)
-        l -= 1
-    print(new_list)
-
-
+    return None
 # TODO: implement
 def challenge2(n):
-    lst = list()
-    lst2 = list()
-    for x in range(1, n+1): #checks for all numbers that can divide n evenly
-        if n % x == 0:
-            lst.append((x, n/x))
-    if len(lst)%2==0:       #if n is even, every entry is doublecounted so half the entries are thrown out
-        for num in range(len(lst)/2):
-            lst2.append(lst[num])
-    elif len(lst)%2 !=0:   #if n is odd, half the entries -1 are thrown out
-        for num in range(len(lst)/2 + 1):
-            lst2.append(lst[num])
-    print(lst2)
-
-    
-
+    return None
+"""
