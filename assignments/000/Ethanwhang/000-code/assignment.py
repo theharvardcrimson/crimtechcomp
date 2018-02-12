069e3794bc17cd37b982614ee5b166b2d934f7db
Ethan Whang <Nox@Ethans-MacBook-Air-3.local>
2018-02-11 19:20:38 -0500

    changes near completion

--- assignments/000/Ethanwhang/000-code/assignment.py
+++ assignments/000/Ethanwhang/000-code/assignment.py
@@ -3,17 +3,14 @@ def say_hello():
 
 # Color: Blue
 
-# prints input 
 def echo_me(msg):
     print(msg)
 
-# TODO: understand and remove
 def string_or_not(d):
     exec(d)
 
-# TODO: understand formatting - can you eliminate the redundancy here?
 def append_msg(msg):
-    print("Your message was: {}.".format(msg))
+    print("Your message should have been: {}!".format(msg))
 
 class QuickMaths():
     def add(self, x, y):
@@ -34,16 +31,15 @@ def increment_by_one(lst):
         new_list.append(x + 1)
     return new_list
 
-# TODO: understand - do we need a return statement here? why?
 def update_name(person, new_name):
     person = {}
     person["name"] = new_name
-
+    
     return person
 
 # TODO: implement - these are still required, but are combinations of learned skills + some
 def challenge1(lst):
-    return None
+    return lst.revere()
 
 # TODO: implement
 def challenge2(n):
