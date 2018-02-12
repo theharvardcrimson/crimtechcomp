df009753cde117263aee5ba0cfcd7224a62e94d9
sepulchre01 <36139072+sepulchre01@users.noreply.github.com>
2018-02-11 22:38:15 -0500

    Update assignment.py

--- assignments/000/000-code/assignment.py
+++ assignments/000/000-code/assignment.py
@@ -1,20 +1,18 @@
 def say_hello():
     print("Hello, world!")
-"""
 
-# this is a singleline comment, PLEASE EDIT ME
-print("I am not executed when in the triple quotes, but once you remove them...PLS REMOVE ME K THX BAI")
+
+# Color: Blue
 
 # TODO: implement
 def echo_me(msg):
+	print(msg)
 
-# TODO: understand and remove
-def string_or_not(d):
-    exec(d)
+
 
 # TODO: understand formatting - can you eliminate the redundancy here?
 def append_msg(msg):
-    print("Your message was: {}.".format(msg))
+    print("Your message should have been: " + msg + "!")
 
 # TODO: understanding classes (an introduction)
 class QuickMaths():
@@ -35,7 +33,7 @@ def increment_by_one(lst):
     new_lst = list()
 
     for x in lst:
-        new_lst.append(x - 1)
+        new_lst.append(x + 1)
 
     return new_lst
 
@@ -47,9 +45,35 @@ def update_name(person, new_name):
 
 # TODO: implement - these are still required, but are combinations of learned skills + some
 def challenge1(lst):
-    return None
+    new_list = list()
+    m = range(len(lst))
+    l = len(lst) - 1
+    for num in m:         #iterates thru each word in lst, starting with the last one
+        word = lst[l]
+        new_word = ""
+        i = len(word) - 1
+        for n in range(len(word)): #iterates thru each letter in word, starting with the last one
+            new_word += word[i]
+            i-= 1
+        new_list.append(new_word)
+        l -= 1
+    print(new_list)
+
 
 # TODO: implement
 def challenge2(n):
-    return None
-"""
+    lst = list()
+    lst2 = list()
+    for x in range(1, n+1): #checks for all numbers that can divide n evenly
+        if n % x == 0:
+            lst.append((x, n/x))
+    if len(lst)%2==0:       #if n is even, every entry is doublecounted so half the entries are thrown out
+        for num in range(len(lst)/2):
+            lst2.append(lst[num])
+    elif len(lst)%2 !=0:   #if n is odd, half the entries -1 are thrown out
+        for num in range(len(lst)/2 + 1):
+            lst2.append(lst[num])
+    print(lst2)
+
+    
+
