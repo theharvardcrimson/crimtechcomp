0197a18c4bad04d5d999786b731aad0f159bab53
vli1721 <vincentli@Vincents-MacBook-Pro.local>
2018-02-11 20:59:58 -0500

    finished assignment1:

--- assignments/vincentli/000-code/assignment.py
+++ assignments/vincentli/000-code/assignment.py
@@ -1,55 +1,51 @@
-print("Hello, world!")
+import math
 
-"""
+def say_hello():
+    print("Hello, world!")
 
-# this is a singleline comment, PLEASE EDIT ME
-print("I am not executed when in the triple quotes, but once you remove them...PLS REMOVE ME K THX BAI")
+# Color: blue
 
-# TODO: implement
 def echo_me(msg):
+    print(msg)
 
-# TODO: understand and remove
-def string_or_not(d):
-    exec(d)
-
-# TODO: understand formatting - can you eliminate the redundancy here?
 def append_msg(msg):
-    print("Your message was: {}.".format(msg))
+    print("Your message should have been: {}!".format(msg))
 
-# TODO: understanding classes (an introduction)
 class QuickMaths():
     def add(self, x, y):
-        return None
+        return x + y
 
     def subtract(self, x, y):
-        return None
+        return x - y
 
     def multiply(self, x, y):
-        return None
+        return x * y
 
     def divide(self, x, y):
-        return None
+        return x / y
 
-# TODO: implement - can you do this more efficiently?
 def increment_by_one(lst):
     new_lst = list()
 
     for x in lst:
-        new_lst.append(x - 1)
+        new_lst.append(x + 1)
 
     return new_lst
 
-# TODO: understand - do we need a return statement here? why?
+# We do not need a return statement here because Python passes the actual dictionary object to the function (not a copy) so the changes made in the function are preserved outside the function
 def update_name(person, new_name):
     person["name"] = new_name
 
-    return person
-
-# TODO: implement - these are still required, but are combinations of learned skills + some
 def challenge1(lst):
-    return None
+    ret_lst = []
+    for word in lst:
+        ret_lst.append(word[::-1])
+    ret_lst.reverse()
+    return ret_lst
 
-# TODO: implement
 def challenge2(n):
-    return None
-"""
+    factors = []
+    for i in range(1, int(math.sqrt(n)) + 2):
+        if (n % i) == 0:
+            factors.append((i, n / i))
+    return factors
