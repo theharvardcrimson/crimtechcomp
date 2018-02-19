bc7ab741c43366c2bf70281ca360c2368ec32a16
Ethan Whang <Nox@Ethans-MacBook-Air-3.local>
2018-02-18 22:26:59 -0500

    finished python

--- assignments/001/001-code/assignment/my_app/models.py
+++ assignments/001/001-code/assignment/my_app/models.py
@@ -3,30 +3,30 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     NONE = "None"
-    HIGH SCHOOL = "high school"
+    HIGH_SCHOOL = "high school"
     UNDERGRADUATE = "undergraduate"
     MASTERS = "masters"
     PHD = "phd"
 
     EDUCATION_TYPE = (
         (NONE, "None"),
-        (HIGH SCHOOL, "high school"),
+        (HIGH_SCHOOL, "high school"),
         (UNDERGRADUATE, "undergraduate"),
         (MASTERS, "masters"),
-        (PHD, "phd"))
+        (PHD, "phd"),)
         
     first_name = models.CharField(max_length=100)
-    last_name = models.Charfield(max_lengths=100)
-    birthday = models.Datefield(auto_now=False, auto_now_add=False)
-    favorite_activies = models.Textfield()
-    email = models.Emailfield(max_length=254)
-    education = models.Charfield(max_length=100, choices= EDUCATION_TYPE, default=NONE)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField(auto_now=False, auto_now_add=False)
+    favorite_activies = models.TextField()
+    email = models.EmailField(max_length=254)
+    education = models.CharField(max_length=100, choices= EDUCATION_TYPE, default=NONE)
 
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    published_date = models.Datefield(auto_now=False, auto_now_add=False)
+    published_date = models.DateField(auto_now=False, auto_now_add=False)
     content = models.TextField()
-    writer = models.ForeignKey('self', NULL = True, on_delete=SET_NULL)
+    writer = models.ForeignKey('Author', null=True, on_delete=models.SET_NULL)
 
     
