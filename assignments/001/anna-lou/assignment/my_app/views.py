ab3e983ced10a386da49fc321a8e8b40709b8a7a
Anna <anna@hydra.local>
2018-02-18 21:25:42 -0500

    clean up code

--- assignments/001/anna-lou/assignment/my_app/views.py
+++ assignments/001/anna-lou/assignment/my_app/views.py
@@ -7,16 +7,14 @@ import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world!')
+    return HttpResponse('Hello, world!<br>'
+                        '<a href="/articles">Articles</a><br>'
+                        '<a href="/authors">Authors</a>')
 
 def articles(request):
-    # a = Article(title="title 3", content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, cumque.")
-    # a.save()
     articles = Article.objects.all()
     return render(request, "articles.html", context={"articles": articles})
 
 def authors(request):
-    # a = Author(first_name="first", last_name="last", birthday=datetime.datetime.now(), favorite_activities="sleeping", email="a@a.com", education="none")
-    # a.save()
     authors = Author.objects.all()
     return render(request, "authors.html", context={"authors": authors})
