507688ae32d013b201b6191d041d606640573b59
sophz55 <sophiazheng5@gmail.com>
2018-02-18 23:01:40 -0500

    assignment 000 complete

--- assignments/000/000-code/assignment.py
+++ assignments/000/000-code/assignment.py
@@ -1,12 +1,11 @@
-print("Hello, world!")
+def say_hello():
+    print("Hello, world!")
 
-"""
-
-# this is a singleline comment, PLEASE EDIT ME
-print("I am not executed when in the triple quotes, but once you remove them...PLS REMOVE ME K THX BAI")
+# Color: pink
 
 # TODO: implement
 def echo_me(msg):
+    print(msg)
 
 # TODO: understand and remove
 def string_or_not(d):
@@ -14,28 +13,28 @@ def string_or_not(d):
 
 # TODO: understand formatting - can you eliminate the redundancy here?
 def append_msg(msg):
-    print("Your message was: {}.".format(msg))
+    print("Your message should have been: {}!".format(msg))
 
 # TODO: understanding classes (an introduction)
 class QuickMaths():
     def add(self, x, y):
-        return None
+        return x + y
 
     def subtract(self, x, y):
-        return None
+        return x - y
 
     def multiply(self, x, y):
-        return None
+        return x * y
 
     def divide(self, x, y):
-        return None
+        return x / y
 
 # TODO: implement - can you do this more efficiently?
 def increment_by_one(lst):
     new_lst = list()
 
     for x in lst:
-        new_lst.append(x - 1)
+        new_lst.append(x + 1)
 
     return new_lst
 
@@ -47,9 +46,22 @@ def update_name(person, new_name):
 
 # TODO: implement - these are still required, but are combinations of learned skills + some
 def challenge1(lst):
-    return None
+    new_lst = list()
+    for x in lst:
+        l = len(x)
+        s = x[l - 1]
+        for i in range(1, l):
+            s += x[l - i - 1]
+        new_lst.append(s)
+    return new_lst
 
 # TODO: implement
 def challenge2(n):
-    return None
-"""
+    lst = list()
+    x1 = 1
+    while (x1 <= n / x1):
+        if (n % x1 == 0):
+            lst.append((x1, n / x1))
+        x1 += 1
+    return lst
+
