6fd4e1e99665188a742373ea5b492c97c1587c02
Ethan Whang <Nox@Ethans-MacBook-Air-3.local>
2018-02-18 22:04:00 -0500

    all yeah

--- assignments/001/001-code/assignment/my_app/models.py
+++ assignments/001/001-code/assignment/my_app/models.py
@@ -2,16 +2,29 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
+    NONE = "None"
+    HIGH SCHOOL = "high school"
+    UNDERGRADUATE = "undergraduate"
+    MASTERS = "masters"
+    PHD = "phd"
+
+    EDUCATION_TYPE = (
+        (NONE, "None"),
+        (HIGH SCHOOL, "high school"),
+        (UNDERGRADUATE, "undergraduate"),
+        (MASTERS, "masters"),
+        (PHD, "phd"))
+        
     first_name = models.CharField(max_length=100)
     last_name = models.Charfield(max_lengths=100)
     birthday = models.Datefield(auto_now=False, auto_now_add=False)
     favorite_activies = models.Textfield()
     email = models.Emailfield(max_length=254)
-    education = models.Charfield(max_length=100, choices=("None", "high school", "undergraduate", "masters", "phd"), default='1')
+    education = models.Charfield(max_length=100, choices= EDUCATION_TYPE, default=NONE)
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=100)
     published_date = models.Datefield(auto_now=Flase, auto_now_add=False)
     content = models.TextField()
     writer = models.ForeignKey('self', NULL = True, on_delete=SET_NULL)
