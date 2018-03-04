ce64debebca1335090012d0045a7f7ed0d94ca04
Schiststorm <james_conant@college.harvard.edu>
2018-03-04 11:25:18 -0500

    finished assignment 001

--- assignments/001/001-code/example_structure/assignment/assignment/settings.py
+++ assignments/001/001-code/example_structure/assignment/assignment/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = '*i#@s=88ylfg_#q0-xrwkrbogtk3!-y!w#21ju6d4t_(ca(s-^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app',
 )
 
 MIDDLEWARE_CLASSES = (
