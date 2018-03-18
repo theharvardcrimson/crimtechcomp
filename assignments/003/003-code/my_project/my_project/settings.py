51e6b151ad63b812e7d5183d6b716d118bc6d121
cindyz8735 <cindyz8735@gmail.com>
2018-03-18 13:08:15 -0400

    dfghshrstetw

--- assignments/003/003-code/my_project/my_project/settings.py
+++ assignments/003/003-code/my_project/my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6-4wci*!+8tf!h4mt2a+ue&13__r+))fprwtp97--z8oesyr1o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app',
 )
 
 MIDDLEWARE_CLASSES = (
--- assignments/003/003-code/my_project/my_project/urls.py
+++ assignments/003/003-code/my_project/my_project/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from my_app import urls as my_app_urls
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'my_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^', include('my_app_urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
