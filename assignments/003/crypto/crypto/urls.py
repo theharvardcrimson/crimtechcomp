66e85644160af21660ad9216272b7fb91834d084
Cindy Zhang <cindyz8735@gmail.com>
2018-03-18 16:05:33 -0700

    aesrgfd

--- assignments/003/crypto/crypto/urls.py
+++ assignments/003/crypto/crypto/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from encode import urls as encode_urls
+from decode import urls as decode_urls
+
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'crypto.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^encode/.*', encode_urls),
+    url(r'^decode/.*', decode_urls),
 ]
