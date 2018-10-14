26298c2c7e4be66de6345af38b24c3d872dc5676
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2018-10-14 14:25:58 -0400

    added dummy assignment.py

---
+++ assignments/001/assignment.py
@@ -0,0 +1,2 @@
+# I'm glad you looked here, but actually this file is totally useless! Sorry! You won't actually be using this file in any way whatsoever, so please do just close/delete/ignore it!
+
